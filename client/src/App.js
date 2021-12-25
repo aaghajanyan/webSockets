f0fb65f2b7d9b4e80570e685c6da20659865adc2
aaghajanyan <aaghajanyan>
2021-12-26 00:23:27 +0400

    Added web-socket repo

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './app.css'
+import LongPulling from "./components/LongPulling";
+import EventSourcing from "./components/EventSourcing";
+import WebSock from "./components/WebSock";
+
+function App() {
+
+  return (
+      <div>
+        <WebSock/>
+      </div>
+  )
+}
+
+
+export default App;
---
+++ server/websocket.js
@@ -0,0 +1,27 @@
+const ws = require('ws');
+const PORT = 5000;
+
+const wss = new ws.Server({
+    port: PORT,
+}, () => console.log(`Server started on 5000`))
+
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function (message) {
+        message = JSON.parse(message)
+        switch (message.event) {
+            case 'message':
+                broadcastMessage(message)
+                break;
+            case 'connection':
+                broadcastMessage(message)
+                break;
+        }
+    })
+})
+
+function broadcastMessage(message, id) {
+    wss.clients.forEach(client => {
+        client.send(JSON.stringify(message))
+    })
+}
